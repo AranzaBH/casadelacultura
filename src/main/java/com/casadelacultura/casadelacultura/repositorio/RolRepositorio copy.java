96d128bf1cd5bffa10c0856c3619bd73fe97a9d5
AranzaBH <araher136@gmail.com>
2024-10-16 20:50:37 -0600

    Create RolRepositorio copy.java

---
+++ src/main/java/com/casadelacultura/casadelacultura/repositorio/RolRepositorio copy.java
@@ -0,0 +1,7 @@
+package com.casadelacultura.casadelacultura.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import com.casadelacultura.casadelacultura.entity.Rol;
+
+public interface RolRepositorio extends CrudRepository<Rol, Integer> {
+}
