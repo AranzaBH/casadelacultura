48fc5378ca40d082abc584eab68cc423a76f2c0b
AranzaBH <araher136@gmail.com>
2024-10-16 20:50:10 -0600

    Create LibrosPorAutorRepositorio.java

---
+++ src/main/java/com/casadelacultura/casadelacultura/repositorio/LibrosPorAutorRepositorio.java
@@ -0,0 +1,7 @@
+package com.casadelacultura.casadelacultura.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import com.casadelacultura.casadelacultura.entity.LibrosPorAutor;
+
+public interface LibrosPorAutorRepositorio extends CrudRepository<LibrosPorAutor, Integer> {
+}
