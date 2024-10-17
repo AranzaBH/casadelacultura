bdc25b85c487d8f052438677167df24f8641f95a
damianbautista <damianbautistaofficial@gmail.com>
2024-10-17 01:19:47 -0600

    Modulos

--- src/main/java/com/casadelacultura/casadelacultura/repositorio/UsuarioRepositorio copy.java
+++
@@ -1,7 +0,0 @@
-package com.casadelacultura.casadelacultura.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-import com.casadelacultura.casadelacultura.entity.Usuario;
-
-public interface UsuarioRepositorio extends CrudRepository<Usuario, Integer> {
-}
