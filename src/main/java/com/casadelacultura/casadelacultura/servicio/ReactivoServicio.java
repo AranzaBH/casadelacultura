050de8c4b9116753463d2155b5aea8cf6da616cb
damianbautista <damianbautistaofficial@gmail.com>
2024-12-01 14:03:27 -0600

    Correccion cuestionario

--- src/main/java/com/casadelacultura/casadelacultura/servicio/ReactivoServicio.java
+++ src/main/java/com/casadelacultura/casadelacultura/servicio/ReactivoServicio.java
@@ -2,6 +2,8 @@ package com.casadelacultura.casadelacultura.servicio;
 
 import org.springframework.stereotype.Service;
 import com.casadelacultura.casadelacultura.entity.Reactivo;
+import com.casadelacultura.casadelacultura.excepciones.GlobalExceptionNoEncontrada;
+import com.casadelacultura.casadelacultura.repositorio.CuestionarioRepositorio;
 import com.casadelacultura.casadelacultura.repositorio.ReactivoRepositorio;
 
 import lombok.*;
@@ -10,6 +12,7 @@ import lombok.*;
 @Service
 public class ReactivoServicio {
     private final ReactivoRepositorio reactivoRepositorio;
+    private final CuestionarioRepositorio cuestionarioRepositorio;
 
     // Obtener todos los reactivos
     public Iterable<Reactivo> listarReactivos() {
@@ -18,17 +21,27 @@ public class ReactivoServicio {
 
     // Obtener un reactivo por ID
     public Reactivo obtenerReactivoPorId(Long idReactivo) {
-        return reactivoRepositorio.findById(idReactivo).orElse(null);
+        return reactivoRepositorio.findById(idReactivo).orElseThrow(() -> new GlobalExceptionNoEncontrada("No se encontro el reactivo con el ID: " + idReactivo));
     }
 
     // Crear un nuevo reactivo
     public Reactivo crearReactivo(Reactivo reactivo) {
+        if (reactivo.getCuestionario() == null ||
+        !cuestionarioRepositorio.existsById(reactivo.getCuestionario().getIdCuestionario())
+        ) {
+            throw new GlobalExceptionNoEncontrada("Cuestionario con ID " +reactivo.getCuestionario().getIdCuestionario() + " no enonctrado.");
+            
+        }
         return reactivoRepositorio.save(reactivo);
     }
 
     // Actualizar un reactivo existente
     public Reactivo actualizarReactivo(Long idReactivo, Reactivo formulario) {
         Reactivo reactivoFromDB = obtenerReactivoPorId(idReactivo);
+        if (formulario.getCuestionario() == null 
+        || !cuestionarioRepositorio.existsById(formulario.getCuestionario().getIdCuestionario())) {
+            throw new GlobalExceptionNoEncontrada("Cuestionario con ID:  " + formulario.getCuestionario().getIdCuestionario() + " no encontrado.");
+        }
         reactivoFromDB.setPregunta(formulario.getPregunta());
         reactivoFromDB.setRespuestaCorrecta(formulario.getRespuestaCorrecta());
         reactivoFromDB.setRespuesta1(formulario.getRespuesta1());
@@ -38,6 +51,9 @@ public class ReactivoServicio {
         reactivoFromDB.setRespuesta5(formulario.getRespuesta5());
         reactivoFromDB.setCuestionario(formulario.getCuestionario());
         return reactivoRepositorio.save(reactivoFromDB);
+
+        
+        
         
     }
 
